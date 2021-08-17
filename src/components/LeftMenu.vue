cb8cf6b9c16de9793b2358a1a9bf29c064f3745a
Chance Hudson <jchancehud@gmail.com>
2021-08-16 21:14:47 -0500

    feat: left menu selected items

--- src/components/LeftMenu.vue
+++ src/components/LeftMenu.vue
@@ -3,33 +3,68 @@
     <div class="header-item">
       Zkopru
     </div>
-    <div class="item selected">
+    <div
+      :class="`item ${isWallet && 'selected'}`"
+      v-on:click="$router.push('/wallet')"
+    >
       <div class="image-container">
-        <img class="image-icon" :src="require('../../assets/wallet_icon.svg')" />
+        <ColorImage
+          class="image-icon"
+          :src="require('../../assets/wallet_icon.svg')"
+          :color="isWallet ? 'white' : '#95A7AE'"
+        />
       </div>
       <div>My Wallet</div>
     </div>
-    <div class="item">
+    <div
+      :class="`item ${isDeposit && 'selected'}`"
+      v-on:click="$router.push('/wallet/deposit')"
+    >
       <div class="image-container">
-        <img class="image-icon" :src="require('../../assets/deposit_icon.svg')" />
+        <ColorImage
+          class="image-icon"
+          :src="require('../../assets/deposit_icon.svg')"
+          :color="isDeposit ? 'white' : '#95A7AE'"
+        />
       </div>
       <div>Deposit</div>
     </div>
-    <div class="item">
+    <div
+      :class="`item ${isWithdraw && 'selected'}`"
+      v-on:click="$router.push('/wallet/withdraw')"
+    >
       <div class="image-container">
-        <img class="image-icon" :src="require('../../assets/withdraw_icon.svg')" />
+        <ColorImage
+          class="image-icon"
+          :src="require('../../assets/withdraw_icon.svg')"
+          :color="isWithdraw ? 'white' : '#95A7AE'"
+        />
       </div>
       <div>Withdraw</div>
     </div>
-    <div class="item">
+    <div
+      :class="`item ${isHistory && 'selected'}`"
+      v-on:click="$router.push('/wallet/history')"
+    >
       <div class="image-container">
-        <img class="image-icon" :src="require('../../assets/history_icon.svg')" />
+        <ColorImage
+          class="image-icon"
+          :src="require('../../assets/history_icon.svg')"
+          :color="isHistory ? 'white' : '#95A7AE'"
+        />
       </div>
       <div>History</div>
     </div>
-    <div class="item">
+    <div
+      :class="`item ${isTokenLibrary && 'selected'}`"
+      v-on:click="$router.push('/wallet/library')"
+    >
       <div class="image-container">
-        <img class="image-icon" :src="require('../../assets/token_library_icon.svg')" />
+        <ColorImage
+          class="image-icon"
+          :src="require('../../assets/token_library_icon.svg')"
+          :color="isTokenLibrary ? 'white' : '#95A7AE'"
+        />
       </div>
       <div>Token Library</div>
     </div>
@@ -38,12 +73,32 @@
 <script>
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import ColorImage from './ColorImage'
 
 @Component({
   name: 'LeftMenu',
+  components: { ColorImage, },
+  computed: {
+    isWallet() {
+      return this.$route.path === '/wallet'
+    },
+    isDeposit() {
+      return this.$route.path.startsWith('/wallet/deposit')
+    },
+    isWithdraw() {
+      return this.$route.path.startsWith('/wallet/withdraw')
+    },
+    isHistory() {
+      return this.$route.path.startsWith('/wallet/history')
+    },
+    isTokenLibrary() {
+      return this.$route.path.startsWith('/wallet/tokens')
+    },
+  }
 })
 export default class LeftMenu extends Vue {
-
+  mounted() {
+  }
 }
 </script>
 <style scoped>
@@ -71,6 +126,7 @@ export default class LeftMenu extends Vue {
 }
 .selected {
   background-color: #192C35;
+  color: white;
 }
 .image-container {
   min-width: 30px;
