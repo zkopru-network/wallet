95605bff703d48ffb5ef284f4436ad5d8ac51549
Chance Hudson <jchancehud@gmail.com>
2021-09-17 23:45:16 -0500

    feat: cache static assets

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const app = express()
 const serve = (_path) => express.static(path.resolve(__dirname, _path))
 
 app.use(compression({ threshold: 0 }))
+app.use((req, res, next) => {
+  const mimes = [
+    'ttf',
+    'otf',
+    'png',
+    'svg',
+    'jpg',
+    'jpeg',
+  ]
+  for (const mime of mimes) {
+    if (req.path.endsWith(mime)) {
+      res.set('Cache-Control', 'max-age=31536000')
+      return next()
+    }
+  }
+  next()
+})
 app.use('/', serve('./build'))
 app.use(microcache.cacheSeconds(1, req => req.originalUrl))
 
