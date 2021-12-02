a0e5800dcf9d6984eef5b70242abb13a1ae6686f
Chance Hudson <jchancehud@gmail.com>
2021-12-02 02:11:37 -0600

    fix: round decimals off of wei

--- src/utils/wei.js
+++ src/utils/wei.js
@@ -18,7 +18,7 @@ export function toWei(amount) {
   if (decimalIndex !== -1) {
     decimalCount = amount.toString().length - decimalIndex
   }
-  const baseAmount = +amount.toString() * (10 ** decimalCount)
+  const baseAmount = Math.floor(+amount.toString() * (10 ** decimalCount))
   const wei = new BN(baseAmount.toString()).mul(new BN('10').pow(new BN(18 - decimalCount)))
   return wei.toString()
 }
